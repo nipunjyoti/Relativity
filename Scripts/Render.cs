86d90734387c9c555d355c0524fec41047a72f35
Nipun Jyoti Pathak <73539736+nipunjyoti@users.noreply.github.com>
2024-10-25 15:13:58 +0530

    Add files via upload

---
+++ Scripts/Render.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Render : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Awake()
+    {
+        Mesh mesh = GetComponent<MeshFilter>().mesh;
+        mesh.RecalculateBounds();
+        mesh.bounds = new Bounds(Vector3.zero, 1000f * Vector3.one);
+    }
+
+}
